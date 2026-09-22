f7572c7a44f789b351767ad36269240c26f422ef
Miguel Re Ibarra <m.reibarra22@gmail.com>
2026-09-22 19:38:46 +0200

    f

--- Ejercicio_15.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $generos = ['Terror', 'Fantasía', 'Acción', 'Comedia', 'Infantil'];
-    $generos[] = 'Ciencia Ficción';
-    $generos[2] = 'Didácticos';
-    unset($generos[0]);
-    foreach ($generos as $genero){
-        echo $genero . "<br>";
-    }
-?>
