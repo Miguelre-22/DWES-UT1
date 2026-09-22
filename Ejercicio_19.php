12b29fe157e4e421141d194fe4cafae1f698d143
Miguel Re Ibarra <m.reibarra22@gmail.com>
2026-09-22 19:37:01 +0200

    Commit del 15 al 19

---
+++ Ejercicio_19.php
@@ -0,0 +1,31 @@
+<?php
+    $libros = [
+        [
+            'titulo' => 'Don Quijote de la Mancha',
+            'autor' => 'Miguel de Cervantes',
+            'paginas' => 458,
+            'diponible' => true,
+        ],
+        [
+            'titulo' => 'Las tumbas de Atuán',
+            'autor' => 'Ursula K. Le Guin',
+            'paginas' => 800,
+            'diponible' => true,
+        ],
+        [
+            'titulo' => 'Orgullo y prejuicio',
+            'autor' => 'Jane Austen',
+            'paginas' => 542,
+            'diponible' => true,
+        ],
+        [
+            'titulo' => 'Cien años de soledad',
+            'autor' => 'Gabriel García Márquez',
+            'paginas' => 558,
+            'diponible' => false,
+        ]
+    ];
+
+    
+
+?>
